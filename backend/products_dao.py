2f645c6913ae183fdbde42ff30787c38bc40bb05
JBras <52011942+BrasJ@users.noreply.github.com>
2025-06-26 16:46:18 -0700

    Create backend server routes
    Added new backend server routes
    -getUOM
    -insertProduct
    -getAllOrders
    -insertOrder
    -deleteProduct

--- backend/products_dao.py
+++ backend/products_dao.py
@@ -5,20 +5,16 @@ def get_all_products(connection):
     cursor = connection.cursor()
     query = "SELECT products.product_id, products.name, products.uom_id, products.price_per_unit, uom.uom_name FROM e_commerce_app.products inner join uom on products.uom_id=uom.uom_id;"
     cursor.execute(query)
-
+    
     response = []
-
     for (product_id, name, uom_id, price_per_unit, uom_name) in cursor:
-        response.append(
-            {
+        response.append({
                 'product_id': product_id,
                 'name': name,
                 'uom_id': uom_id,
                 'price_per_unit': price_per_unit,
                 'uom_name': uom_name
-            }
-        )
-
+        })
     return response
 
 def insert_new_product(connection, product):
@@ -27,6 +23,7 @@ def insert_new_product(connection, product):
              "(name, uom_id, price_per_unit)"
              "VALUES (%s, %s, %s)")
     data = (product['product_name'], product['uom_id'], product['price_per_unit'])
+    
     cursor.execute(query, data)
     connection.commit()
 
@@ -37,6 +34,8 @@ def delete_product(connection, product_id):
     query = ("DELETE FROM products WHERE product_id=" + str(product_id))
     cursor.execute(query)
     connection.commit()
+
+    return cursor.lastrowid
 
 
 if __name__ == '__main__':
